c1e1a3d11b4c2a11280d6ae5652b731dc3c4fb7d
zjm <1076115376@qq.com>
2022-08-22 09:31:09 +0800

    3

---
+++ src/list/list.rs
@@ -0,0 +1,6 @@
+#[allow(dead_code)]
+pub fn list() {
+    let _v:Vec<i32> = vec![];
+    let v = vec![1,2,3];
+    println!("{:?}",v)
+}
--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,14 @@ mod english;
 mod traits;
 mod directory;
 mod demof;
+mod list;
 // use std::io::Write;
 // use cache1::cache2::d;
 // use demof::demo::demo::demofs;
-use directory::{closure};
+// use directory::{closure};
+
 fn main() {
-    closure::demo::closure_7()
+
 }
 
 
