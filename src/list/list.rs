118a8b0f6ba19bbe3c71a056dabc11f53f225142
zjm <1076115376@qq.com>
2022-08-24 15:58:51 +0800

    4

--- src/list/list.rs
+++ src/list/list.rs
@@ -1,6 +1,74 @@
 #[allow(dead_code)]
 pub fn list() {
-    let _v:Vec<i32> = vec![];
-    let v = vec![1,2,3];
-    println!("{:?}",v)
+    let mut v1:Vec<usize> = vec![];
+    push(&mut v1, 50_000_000);
+
+    let mut v2:Vec<usize> = vec![];
+    v2.reserve(50_000_000);
+    push(&mut v2, 50_000_000)
+}
+use std::time::SystemTime;
+fn push(v:&mut Vec<usize>, total: usize) {
+    let e1 = SystemTime::now();
+
+    for i in 1..total {
+        v.push(i)
+    }
+
+    let e2 = SystemTime::now();
+
+    println!("cost time {:?}", e2.duration_since(e1).unwrap())
+}
+
+use std::collections::hash_map::HashMap;
+#[allow(dead_code)]
+pub fn map() {
+    // 声明
+    let mut come_from = HashMap::new();
+// 插入
+    come_from.insert("WaySLOG", "HeBei");
+    come_from.insert("Marisa", "U.S.");
+    come_from.insert("Mike", "HuoGuo");
+
+// 查找key
+    if !come_from.contains_key("elton") {
+        println!("Oh, 我们查到了{}个人，但是可怜的Elton猫还是无家可归", come_from.len());
+    }
+
+// 根据key删除元素
+    come_from.remove("Mike");
+    println!("Mike猫的家乡不是火锅！不是火锅！不是火锅！虽然好吃！");
+
+// 利用get的返回判断元素是否存在
+    let who = ["MoGu", "Marisa"];
+    for person in &who {
+        match come_from.get(person) {
+            Some(location) => println!("{} 来自: {}", person, location),
+            None => println!("{} 也无家可归啊.", person),
+        }
+    }
+
+// 遍历输出
+    println!("那么，所有人呢？");
+    for (name, location) in &come_from {
+        println!("{}---来自: {}", name, location);
+    }
+}
+#[allow(dead_code)]
+pub fn map1() {
+    let mut l = HashMap::new();
+
+    for i in "a short treatise on fungi".chars() {
+        if i == ' ' {
+            continue
+        }
+        let c = l.entry(i).or_insert(0);
+        *c += 1;
+    }
+
+    println!("{:?}", l)
+}
+#[allow(dead_code)]
+pub fn iter(){
+    IntoIterator::into_iter()
 }
--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,9 @@ mod list;
 // use cache1::cache2::d;
 // use demof::demo::demo::demofs;
 // use directory::{closure};
-
+use list::list::map1;
 fn main() {
-
+    map1()
 }
 
 
